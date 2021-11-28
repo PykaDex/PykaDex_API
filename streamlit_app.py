09546c7cbf6d35441e3ecbd899ebf96eab14cf41
Shellywell123 <44142139+Shellywell123@users.noreply.github.com>
2021-11-28 18:29:30 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from pytorch_prediction_pokemon_api.py import get_prediction
+from pytorch_prediction_pokemon_api import get_prediction
 
 
 ##########################################
